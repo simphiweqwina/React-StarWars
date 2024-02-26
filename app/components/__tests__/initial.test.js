759acb8e2f3a27d4bef30fe20820674cc005f6db
Simphiwe Simon Qwina <qwinas@LEGAL.REGN.NET>
2024-02-26 11:23:21 +0200

    added configs for jest and cypress

---
+++ app/components/__tests__/initial.test.js
@@ -0,0 +1,5 @@
+import {render,screen,cleanup} from '@testing-library/react'
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(1+2).toBe(3);
+});
---
+++ cypress.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "cypress";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+
+module.exports = defineConfig({
+  e2e: {
+    async setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+
+      on("file:preprocessor", bundler);
+      await addCucumberPreprocessorPlugin(on, config);
+
+      return config;
+    },
+    specPattern: "cypress/e2e/features/*.feature",
+    baseUrl: "http://localhost:3000/",
+    chromeWebSecurity: false,
+  },
+});
+
